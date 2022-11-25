0fd491f7881c231a8a101d24bc54728b120e40dc
knutc <knutc.kodehode@gmail.com>
2022-11-25 13:14:11 +0100

    styled application

---
+++ src/components/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const QuestionContainer = styled.div`
+	border-bottom: 1px solid #dbdef0;
+	@media (max-width: 780px) {
+		display: flex;
+		flex-direction: column;
+	}
+`;
+
+export const AnswerBtn = styled.button`
+	border-radius: 10px;
+	padding: 4px 12px;
+	min-width: 65px;
+	margin-right: 13px;
+	color: inherit;
+	background: none;
+	:disabled {
+		color: #8f94af;
+		border: 2px solid #c0c3d0;
+		background-color: '';
+	}
+`;
