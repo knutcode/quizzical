6593c9c09a56f26457b4afb6eb65328a993bd649
knutc <knutc.kodehode@gmail.com>
2022-11-29 10:41:23 +0100

    refactor/logic in App moved to custom hook

--- src/App.js
+++ src/App.js
@@ -1,68 +1,11 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { decode } from 'html-entities';
 import { QuizzCard } from './components/QuizzCard';
-import { nanoid } from 'nanoid';
 import { StartScreen } from './components/StartScreen';
 import { SVGYellow } from './components/SVGYellow';
 import { SVGBlue } from './components/SVGBlue';
+import useQuizzGame from './hooks/useQizzGame';
 
 function App() {
-	const [quizzData, setQuizzData] = useState();
-	const [points, setPoints] = useState(0);
-	const [gameState, setGameState] = useState({
-		isActive: false,
-		isOver: false,
-	});
-	const [category, setCategory] = useState({ value: '', topic: '' });
-
-	const fetchData = () => {
-		setPoints(0);
-		setGameState((prevState) => {
-			return { ...prevState, isOver: false };
-		});
-		setQuizzData();
-		axios
-			.get(`https://opentdb.com/api.php?amount=5&type=multiple&category=${category.value}`)
-			.then((res) => {
-				return res.data.results;
-			})
-			.then((results) =>
-				setQuizzData(
-					results.map((item) => {
-						return {
-							id: nanoid(),
-							question: decode(item.question),
-							correctAnswer: item.correct_answer,
-							answers: [...item.incorrect_answers, item.correct_answer]
-								.map((value) => ({ value, sort: Math.random() }))
-								.sort((a, b) => a.sort - b.sort)
-								.map(({ value }) => value),
-						};
-					})
-				)
-			)
-			.then(
-				setGameState((prevState) => {
-					return { ...prevState, isActive: true };
-				})
-			);
-	};
-
-	const countPoints = () => {
-		setPoints((prevState) => {
-			return prevState + 1;
-		});
-	};
-
-	const changeCategory = (event) => {
-		setCategory((prevCategory) => {
-			return {
-				...prevCategory,
-				value: event.target.value,
-			};
-		});
-	};
+	const { gameState, setGameState, points, countPoints, category, changeCategory, fetchData, quizzData } = useQuizzGame();
 
 	return (
 		<>
